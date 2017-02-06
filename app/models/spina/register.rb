10d26e212a76282e536d2fc1da8fe41ca01b3792
Dan Mitchell <dan@digitalreflow.co.uk>
2017-02-06 12:08:43 +0000

    Update list of authorities

--- app/models/spina/register.rb
+++ app/models/spina/register.rb
@@ -4,9 +4,27 @@ module Spina
     before_validation :set_slug
 
     CURRENT_PHASES = ['Backlog', 'Discovery', 'Alpha', 'Beta', 'Live']
-    AUTHORITIES = ['Companies House', 'Ministry of Justice', 'Department for Education', 'Foreign and Commonwealth Office', 'Welsh government',
-              'Government Digital Service', 'Department for Work and Pensions', 'Home Office', 'Valuation Office Agency', 'Cabinet Office',
-              'NHS', 'Government Statistical Service', 'Department for Communities and Local Government', 'Department for Environment, Food and Rural Affairs']
+    AUTHORITIES =
+                [
+                  'Companies House',
+                  'Ministry of Justice',
+                  'Department for Education',
+                  'Foreign and Commonwealth Office',
+                  'Welsh government',
+                  'Government Digital Service',
+                  'Department for Work and Pensions',
+                  'Home Office',
+                  'Valuation Office Agency',
+                  'Cabinet Office',
+                  'NHS',
+                  'Government Statistical Service',
+                  'Department for Communities and Local Government',
+                  'Food Standards Agency',
+                  'Department for Environment, Food and Rural Affairs',
+                  'General Register Office',
+                  'National Offender Management Service',
+                  'Animal and Plant Health Agency'
+                ]
 
     validates_presence_of :name, :register_phase, :authority
     validates_uniqueness_of :name
