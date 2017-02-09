df91cd570ae7d34b10ec4d40ebbd0f6238f8be2e
Dan Mitchell <dan@digitalreflow.co.uk>
2017-02-09 15:11:52 +0000

    Add product page content

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   mount Spina::Engine => '/'
 
   Spina::Engine.routes.draw do
-    resources :registers, only: [:show, :index]
+    resources :registers, only: [:show, :index], path: "register-status"
 
     namespace :admin do
       resources :registers, except: [:show]
