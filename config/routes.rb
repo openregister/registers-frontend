54c3ecf73af561b7ef4812de2a6041a84222d37b
Dan Mitchell <dan@digitalreflow.co.uk>
2017-02-10 11:35:14 +0000

    Put register status url back to registers

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   mount Spina::Engine => '/'
 
   Spina::Engine.routes.draw do
-    resources :registers, only: [:show, :index], path: "register-status"
+    resources :registers, only: [:show, :index]
 
     namespace :admin do
       resources :registers, except: [:show]
