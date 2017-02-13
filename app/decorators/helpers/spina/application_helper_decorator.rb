31e999057ca3e589c60cd1b3ad75105e531f587b
Dan Mitchell <dan@digitalreflow.co.uk>
2017-02-13 15:45:26 +0000

    Preserve url params when sorting table

--- app/decorators/helpers/spina/application_helper_decorator.rb
+++ app/decorators/helpers/spina/application_helper_decorator.rb
@@ -5,7 +5,7 @@ module Spina
       direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
       css_class = sort_direction == "asc" ? "sorting-up" : "sorting-down"
       css_class = column == sort_column ? css_class : ""
-      link_to title, { column: column, direction: direction }, class: css_class
+      link_to title, request.query_parameters.merge({ column: column, direction: direction }), class: css_class
     end
   end
 end
