a603b88452dbcfd738bece4ce9d9b0f269a31df8
Dan Mitchell <dan@digitalreflow.co.uk>
2017-02-14 14:06:05 +0000

    Refactor government organisations helper method
    This commit also moves the helper methods out of the application
    controller decorator and into the helper method decorator

--- app/decorators/helpers/spina/application_helper_decorator.rb
+++ app/decorators/helpers/spina/application_helper_decorator.rb
@@ -1,3 +1,5 @@
+require 'govspeak'
+
 module Spina
   ApplicationHelper.module_eval do
     def sort_link(column, title = nil)
@@ -6,6 +8,35 @@ module Spina
       css_class = sort_direction == "asc" ? "sorting-up" : "sorting-down"
       css_class = column == sort_column ? css_class : ""
       link_to title, request.query_parameters.merge({ column: column, direction: direction }), class: css_class
+    end
+
+    def govspeak(text)
+      Govspeak::Document.new(text).to_html.html_safe
+    end
+
+    def crest_class_name(authority)
+      case authority
+      when "home-office"
+        "logo-with-crest crest-ho"
+      when "ministry-of-defence"
+        "logo-with-crest crest-mod"
+      when "hm-revenue-customs"
+        "logo-with-crest crest-hmrc"
+      when "welsh-government"
+        "logo-with-crest crest-wales"
+      when "department-for-business-innovation-and-skills"
+        "logo-with-crest crest-bis"
+      when "the-office-of-the-leader-of-the-house-of-commons"
+        "logo-with-crest crest-portcullis"
+      when "office-of-the-advocate-general-for-scotland"
+        "logo-with-crest crest-so"
+      when "uk-atomic-energy-authority"
+        "logo-with-crest crest-ukaea"
+      when "nhs", "government-digital-service"
+        " "
+      else
+        "logo-with-crest crest-org"
+      end
     end
   end
 end
