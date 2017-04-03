722a920c4a2872e7480c8610560d68371dfce149
Dan Mitchell <dan@digitalreflow.co.uk>
2017-04-03 23:03:01 +0100

    Add cleaner support path

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
     resources :registers, only: [:show, :index]
     resources :supports
 
+    get '/support', to: 'supports#new', as: 'get_support'
+
     namespace :admin do
       resources :registers, except: [:show]
     end
