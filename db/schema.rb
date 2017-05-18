c9c855702bada3672a4f7f7b7289514a34eb8c8a
Dan Mitchell <dan@digitalreflow.co.uk>
2017-05-18 17:44:24 +0100

    Update cms for password reset and to Rails 5.0.3

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170404112957) do
+ActiveRecord::Schema.define(version: 20170518163832) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -241,10 +241,12 @@ ActiveRecord::Schema.define(version: 20170404112957) do
     t.string   "name"
     t.string   "email"
     t.string   "password_digest"
-    t.boolean  "admin",           default: false
-    t.datetime "created_at",                      null: false
-    t.datetime "updated_at",                      null: false
+    t.boolean  "admin",                  default: false
+    t.datetime "created_at",                             null: false
+    t.datetime "updated_at",                             null: false
     t.datetime "last_logged_in"
+    t.string   "password_reset_token"
+    t.datetime "password_reset_sent_at"
   end
 
 end
