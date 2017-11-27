b8196a65136270bc5519f7c7ca3f857456352ed2
fjavierm <javier.marquez@digital.cabinet-office.gov.uk>
2017-11-27 14:54:41 +0000

    Change the search for the update page

--- app/controllers/spina/registers_controller.rb
+++ app/controllers/spina/registers_controller.rb
@@ -33,12 +33,12 @@ module Spina
 
       if params[:status]
         case params[:status]
-        when 'closed'
-          @records = @register_data.get_expired_records
-        when 'current'
-          @records = @register_data.get_current_records
-        when 'all'
-          @records = @register_data.get_records
+          when 'closed'
+            @records = @register_data.get_expired_records
+          when 'current'
+            @records = @register_data.get_current_records
+          when 'all'
+            @records = @register_data.get_records
         end
       else
         @records = @register_data.get_current_records
@@ -53,13 +53,13 @@ module Spina
       @register = Spina::Register.find_by_slug!(params[:id])
       @register_data = @@registers_client.get_register(@register.name.parameterize, @register.register_phase)
 
-      fields = @register_data.get_field_definitions.map{ |field| field[:item]['field'] }
+      fields = @register_data.get_field_definitions.map { |field| field[:item]['fields'] }
       all_records = @register_data.get_records_with_history
       entries_reversed = @register_data.get_entries.reverse
 
       @entries_with_items = entries_reversed.map { |entry|
         records_for_key = all_records[entry[:key]]
-        current_record = records_for_key.detect{ |record| record[:entry_number] == entry[:entry_number] }
+        current_record = records_for_key.detect { |record| record[:entry_number] == entry[:entry_number] }
         previous_record_index = records_for_key.find_index(current_record) - 1
 
         if previous_record_index < 0
@@ -71,9 +71,47 @@ module Spina
 
         { current_record: current_record, previous_record: previous_record, updated_fields: changed_fields, key: entry[:key] }
       }
+
+      filter(params[:search_input]) unless params[:search_input].nil?
     end
 
     private
+
+    def filter(filter_value)
+      @entries_with_items = @entries_with_items.select { |entry|
+        entry[:key].downcase.include?(filter_value.downcase) ||
+          contained?(entry[:previous_record], filter_value) ||
+          contained?(entry[:current_record], filter_value)
+      }
+    end
+
+    def contained?(item, request_value)
+      return false if item.nil?
+
+      item[:item].each { |fields|
+        fields.each do |field_value|
+          if field_value.is_a?(String)
+            return true if include_in_card_1?(field_value, request_value)
+          else
+            return true if include_in_card_n?(field_value, request_value)
+          end
+        end
+      }
+
+      false
+    end
+
+    def include_in_card_n?(field_values, request_value)
+      field_values.each { |field_value|
+        return true if include_in_card_1?(field_value, request_value)
+      }
+
+      false
+    end
+
+    def include_in_card_1?(field_value, request_value)
+      field_value.downcase.include?(request_value.downcase)
+    end
 
     def initialize_client
       @@registers_client ||= RegistersClient::RegistersClientManager.new({ cache_duration: 600 })
