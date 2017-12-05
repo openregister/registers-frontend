60b80b4c8a5ce724c10fa57a9d16c6111246baed
Dan Mitchell <dan@digitalreflow.co.uk>
2017-12-05 14:17:12 +0000

    Only use http auth in production

--- app/decorators/controllers/spina/application_controller_decorator.rb
+++ app/decorators/controllers/spina/application_controller_decorator.rb
@@ -1,3 +1,3 @@
 Spina::ApplicationController.class_eval do
-  http_basic_authenticate_with name: ENV["HTTP_AUTH_USERNAME"], password: ENV["HTTP_AUTH_PASSWORD"]
+  http_basic_authenticate_with name: ENV["HTTP_AUTH_USERNAME"], password: ENV["HTTP_AUTH_PASSWORD"] if Rails.env.production?
 end
