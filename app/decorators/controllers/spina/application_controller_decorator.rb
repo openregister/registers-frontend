9bd058792f8ef2bf9c344b5272581df37d9f1f35
Dan Mitchell <dan@digitalreflow.co.uk>
2017-12-08 10:43:36 +0000

    Add rails HTTP Auth

--- app/decorators/controllers/spina/application_controller_decorator.rb
+++ app/decorators/controllers/spina/application_controller_decorator.rb
@@ -1,4 +1,6 @@
 Spina::ApplicationController.class_eval do
+  http_basic_authenticate_with name: ENV['HTTP_AUTH_USERNAME'], password: ENV['HTTP_AUTH_PASSWORD'] if Rails.env.production?
+
   before_action :initialize_client
 
   def initialize_client
