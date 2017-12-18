aeb4184de78e51844e3eaf617bbe06c709c05a6c
Dan Mitchell <dan@digitalreflow.co.uk>
2017-12-18 17:04:20 +0000

    Drop history page fields and database tables (#91)
    * Drop history page fields and database tables
    * Update seeds file

--- app/models/spina/register.rb
+++ app/models/spina/register.rb
@@ -9,11 +9,6 @@ module Spina
     validates_uniqueness_of :name
     validates :slug, uniqueness: true
 
-    has_many :steps, -> { order('position ASC') }
-    accepts_nested_attributes_for :steps, reject_if: :all_blank, allow_destroy: true
-    has_many :phases, -> { order('position ASC') }
-    accepts_nested_attributes_for :phases, reject_if: :all_blank, allow_destroy: true
-
     scope :by_name, -> { order name: :asc }
     scope :sort_by_phase_name_asc, -> { order("CASE register_phase WHEN 'Beta' THEN 2 WHEN 'Alpha' THEN 3 WHEN 'Discovery' THEN 4 WHEN 'Backlog' THEN 5 END") }
     scope :sort_by_phase_name_desc, -> { order("CASE register_phase WHEN 'Backlog' THEN 1 WHEN 'Discovery' THEN 2 WHEN 'Alpha' THEN 3 WHEN 'Beta' THEN 4 END") }
