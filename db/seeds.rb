91f701a054950e5edfc3a286563e87f216fd9f3f
Dan Mitchell <dan@digitalreflow.co.uk>
2017-12-18 12:26:04 +0000

    Update seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,6 @@ Spina::Register.create(
   name: "Country",
   register_phase: "Beta",
   authority: "Foreign and Commonwealth Office",
-  custodian: "Tony Worron",
-  url: "https://country.beta.openregister.org/",
-  history: "",
 )
 puts "Created Country Register"
 
@@ -16,9 +13,6 @@ Spina::Register.create(
   name: "Territory",
   register_phase: "Beta",
   authority: "Foreign and Commonwealth Office",
-  custodian: "Tony Worron",
-  url: "https://territory.beta.openregister.org/",
-  history: "",
 )
 puts "Created Territory Register"
 
@@ -26,9 +20,6 @@ Spina::Register.create(
   name: "Local authority eng",
   register_phase: "Alpha",
   authority: "Department for Communities and Local Government",
-  custodian: "Mark Coram",
-  url: "https://local-authority-eng.register.gov.uk/",
-  history: "https://registers.cloudapps.digital/registers/local-authority-eng",
 )
 puts "Created Local authority eng Register"
 
@@ -36,8 +27,6 @@ Spina::Register.create(
   name: "School eng",
   register_phase: "Alpha",
   authority: "Department for Education",
-  custodian: "Iain Bradley",
-  url: "https://school-eng.alpha.openregister.org/",
   history: "https://registers.cloudapps.digital/registers/school-eng",
 )
 puts "Created School eng Register"
@@ -46,9 +35,6 @@ Spina::Register.create(
   name: "Prison estate",
   register_phase: "Beta",
   authority: "Ministry of Justice",
-  custodian: "Richard Carling",
-  url: "https://prison-estate.register.gov.uk/",
-  history: "https://registers.cloudapps.digital/registers/prison-estate",
 )
 puts "Created Prison Register"
 
@@ -56,9 +42,6 @@ Spina::Register.create(
   name: "Jobcentre",
   register_phase: "Discovery",
   authority: "Department for Work and Pensions",
-  custodian: "",
-  url: "http://jobcentre.discovery.openregister.org/",
-  history: "",
 )
 puts "Created Jobcentre Register"
 
@@ -66,9 +49,6 @@ Spina::Register.create(
   name: "Government organisation",
   register_phase: "Beta",
   authority: "Cabinet Office",
-  url: "https://government-organisation.register.gov.uk/",
-  custodian: "Neil Williams",
-  history: "https://registers.cloudapps.digital/registers/government-organisation",
 )
 puts "Created Government organisation Register"
 
@@ -76,9 +56,6 @@ Spina::Register.create(
   name: "Government domain",
   register_phase: "Beta",
   authority: "Cabinet Office",
-  url: "https://government-domain.register.gov.uk/",
-  custodian: "Evans Bissessar",
-  history: "https://registers.cloudapps.digital/registers/government-domain",
 )
 puts "Created Government domain Register"
 
