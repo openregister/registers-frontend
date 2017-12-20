96c1ad5685924e1d7a58333e9308624691f4c2d3
Dan Mitchell <dan@digitalreflow.co.uk>
2017-12-20 11:01:31 +0000

    Change column type of description in CMS to match API

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171219103147) do
+ActiveRecord::Schema.define(version: 20171220104627) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -178,7 +178,7 @@ ActiveRecord::Schema.define(version: 20171219103147) do
     t.string "authority"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.text "description"
+    t.string "description"
     t.text "related_registers"
   end
 
