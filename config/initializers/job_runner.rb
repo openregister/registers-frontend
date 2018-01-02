793caa99f3c59fef752777af6f0e12f5adf41dfd
Gideon Goldberg <gidsgoldberg@gmail.com>
2018-01-02 12:46:32 +0000

    add more conditions to job runner

--- config/initializers/job_runner.rb
+++ config/initializers/job_runner.rb
@@ -1,3 +1,3 @@
-unless Rails.env.test?
+unless Rails.env.test? || !ActiveRecord::Base.connection.table_exists?('spina_registers') || File.basename($0) == 'rake'
   PopulateRegisterDataInDbJob.perform_now
 end
