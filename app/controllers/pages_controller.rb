3bb8c63bc070a2d412608650c7ce34a97f0597c8
Dan Mitchell <dan@digitalreflow.co.uk>
2018-01-02 17:29:33 +0000

    No longer need get_ready_to_use_registers on home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@
 class PagesController < ApplicationController
   layout 'layouts/application'
 
-  before_action :get_ready_to_use_registers, only: [:home, :avaliable_registers]
+  before_action :get_ready_to_use_registers, only: :avaliable_registers
 
   def home
     @registers = Spina::Register.all
