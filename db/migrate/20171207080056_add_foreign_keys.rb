6f449db8842fecbdb210f4279122b50b0e1a5c3d
Dan Mitchell <dan@digitalreflow.co.uk>
2018-01-02 17:18:01 +0000

    Remove legacy db migration

--- db/migrate/20171207080056_add_foreign_keys.rb
+++ db/migrate/20171207080056_add_foreign_keys.rb
@@ -1,6 +1,5 @@
 class AddForeignKeys < ActiveRecord::Migration[5.1]
   def change
-    add_column :items, :spina_register_id, :integer
     add_column :entries, :item_id, :integer
     add_column :records, :item_id, :integer
   end
