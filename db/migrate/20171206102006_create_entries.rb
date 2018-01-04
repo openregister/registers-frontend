7052892ab368b8849faae379021bc05d34b48183
Dan Mitchell <dan@digitalreflow.co.uk>
2018-01-04 21:51:43 +0000

    fix migrations

--- db/migrate/20171206102006_create_entries.rb
+++ db/migrate/20171206102006_create_entries.rb
@@ -2,12 +2,12 @@ class CreateEntries < ActiveRecord::Migration[5.1]
   def change
     create_table :entries do |t|
       t.text :hash_value
-      t.integer :entry_number
-      t.integer :previous_entry_number
       t.text :entry_type
       t.text :key
       t.datetime :timestamp
       t.jsonb :data
+      t.bigint :spina_register_id
+      t.index :spina_register_id, name: :index_entry_on_spina_register_id
 
       t.timestamps
     end
--- db/migrate/20171206130059_add_populated_to_registers.rb
+++
@@ -1,5 +0,0 @@
-class AddPopulatedToRegisters < ActiveRecord::Migration[5.1]
-  def change
-    add_column :spina_registers, :populated, :boolean, default: false
-  end
-end
