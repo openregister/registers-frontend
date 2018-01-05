60082d700eeeec42d02da256a9bae80e57365b3e
Gideon Goldberg <gidsgoldberg@gmail.com>
2018-01-05 17:05:10 +0000

    log instead of puts

--- lib/clock.rb
+++ lib/clock.rb
@@ -7,10 +7,10 @@ require File.expand_path('../../config/environment', __FILE__)
 include Clockwork
 
 handler do |job|
-  puts "Running #{job}"
+  Rails.logger.info "Executing: #{job}"
   system(job)
 end
 
-every(1.minute, 'Update database') {
+every(10.minute, 'Update database') {
   `rake registers_frontend:populate_db:fetch_later`
 }
