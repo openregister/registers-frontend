dc5fa4f17f1d9ba570e161ae02f5de6d6cce49db
Gideon Goldberg <gidsgoldberg@gmail.com>
2018-01-16 14:03:37 +0000

    add else condition to logger

--- config/initializers/delayed_job_config.rb
+++ config/initializers/delayed_job_config.rb
@@ -11,4 +11,6 @@ Delayed::Worker.logger ||= if defined?(Rails)
                              Rails.logger
                            elsif defined?(RAILS_DEFAULT_LOGGER)
                              RAILS_DEFAULT_LOGGER
+                           else
+                             Logger.new(File.join(@worker_options[:log_dir], 'delayed_job.log'))
                            end
