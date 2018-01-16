c325a380a8f2a23147cf0f3e8e04c6d4f92eee6d
Gideon Goldberg <gidsgoldberg@gmail.com>
2018-01-16 11:05:02 +0000

    use default rails logger for delayed job

--- config/initializers/delayed_job_config.rb
+++ config/initializers/delayed_job_config.rb
@@ -6,4 +6,9 @@ Delayed::Worker.read_ahead = 10
 Delayed::Worker.default_queue_name = 'default'
 Delayed::Worker.delay_jobs = !Rails.env.test?
 Delayed::Worker.raise_signal_exceptions = :term
-Delayed::Worker.logger = Logger.new(File.join(Rails.root, 'log', 'delayed_job.log'))
+
+Delayed::Worker.logger ||= if defined?(Rails)
+                             Rails.logger
+                           elsif defined?(RAILS_DEFAULT_LOGGER)
+                             RAILS_DEFAULT_LOGGER
+                           end
