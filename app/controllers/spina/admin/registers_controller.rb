9fbb3fcf61afba4eadd2831c2e61b1ae1633b5f1
Dan Mitchell <dan@digitalreflow.co.uk>
2018-02-01 12:17:57 +0000

    Add parentheses around args on add_breadcrumb method

--- app/controllers/spina/admin/registers_controller.rb
+++ app/controllers/spina/admin/registers_controller.rb
@@ -12,16 +12,16 @@ module Spina
       end
 
       def new
-        add_breadcrumb "New #{t('spina.registers.scaffold_name')}", spina.new_admin_register_path
+        add_breadcrumb("New #{t('spina.registers.scaffold_name')}", spina.new_admin_register_path)
         @register = Spina::Register.new
       end
 
       def edit
-        add_breadcrumb @register.name
+        add_breadcrumb(@register.name)
       end
 
       def create
-        add_breadcrumb "New #{t('spina.registers.scaffold_name')}"
+        add_breadcrumb("New #{t('spina.registers.scaffold_name')}")
         @register = Spina::Register.new(register_params)
         if @register.save
           flash.now[:success] = "Successfull saved"
@@ -33,7 +33,7 @@ module Spina
       end
 
       def update
-        add_breadcrumb @register.name
+        add_breadcrumb(@register.name)
         if @register.update_attributes(register_params)
           flash.now[:success] = "Successfull saved"
           redirect_to spina.edit_admin_register_url(@register)
@@ -55,7 +55,7 @@ module Spina
       end
 
       def set_breadcrumb
-        add_breadcrumb t('spina.registers.scaffold_name_plural'), spina.admin_registers_path
+        add_breadcrumb(t('spina.registers.scaffold_name_plural'), spina.admin_registers_path)
       end
 
       def set_government_organisations
