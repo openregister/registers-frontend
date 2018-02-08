f221ca5e4c8e924348447c815a4f639a06880e8f
Gideon Goldberg <gidsgoldberg@gmail.com>
2018-02-08 12:43:15 +0000

    wrap both client library calls instead of monkey patching

--- app/jobs/populate_register_data_in_db_job.rb
+++ app/jobs/populate_register_data_in_db_job.rb
@@ -1,35 +1,22 @@
 class PopulateRegisterDataInDbJob < ApplicationJob
   queue_as :default
 
-
   module Exceptions
     class FrontendInvalidRegisterError < StandardError; end
   end
 
-  module RegisterClientExtensions
-    include Exceptions
-    def refresh_data
-      begin
-        super
-      rescue InvalidRegisterError
-        # If register data is invalid we want to delete existing entries and records to force a full reload
-        register_id = @data_store.instance_variable_get(:@register).id
-        Record.where(register_id: register_id).destroy_all
-        Entry.where(register_id: register_id).destroy_all
-        raise Exceptions::FrontendInvalidRegisterError
-      end
-    end
-  end
-
-  RegistersClient::RegisterClient.module_eval do
-    prepend RegisterClientExtensions
-  end
-
   def perform(register)
     Delayed::Worker.logger.info("Updating #{register.name} in database")
+    begin
     # Initialize client and download / update data
-    register_client = @@registers_client.get_register(register.name.parameterize, register.register_phase.downcase, PostgresDataStore.new(register))
-    register_client.refresh_data
+      register_client = @@registers_client.get_register(register.name.parameterize, register.register_phase.downcase, PostgresDataStore.new(register))
+      register_client.refresh_data
+    rescue InvalidRegisterError
+      # If register data is invalid we want to delete existing entries and records to force a full reload
+      Record.where(register_id: register.id).destroy_all
+      Entry.where(register_id: register.id).destroy_all
+      raise Exceptions::FrontendInvalidRegisterError
+    end
 
     metadata_records = Record.where(register_id: register.id, entry_type: :system)
     fields = metadata_records.select { |record| record.key.start_with?("field:") }
