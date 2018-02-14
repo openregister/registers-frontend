35544e43e7da53c23028b33f995bdecefaa9744c
Dan Mitchell <dan@digitalreflow.co.uk>
2018-02-14 15:18:32 +0000

    Add register url from API

--- app/jobs/populate_register_data_in_db_job.rb
+++ app/jobs/populate_register_data_in_db_job.rb
@@ -10,6 +10,7 @@ class PopulateRegisterDataInDbJob < ApplicationJob
     begin
     # Initialize client and download / update data
       register_client = RegistersClientWrapper.registers_client.get_register(register.name.parameterize, register.register_phase.downcase, PostgresDataStore.new(register))
+      register_url = register_client.instance_variable_get(:@register_url)
       register_client.refresh_data
     rescue InvalidRegisterError => e
     # If register data is invalid we want to delete existing entries and records to force a full reload
@@ -23,6 +24,7 @@ class PopulateRegisterDataInDbJob < ApplicationJob
     metadata_records = Record.where(register_id: register.id, entry_type: :system)
     fields = metadata_records.select { |record| record.key.start_with?("field:") }
     register.fields = fields.map { |field| field.data['field'] }.join(',')
+    register.url = register_url
     register.save
   end
 end
