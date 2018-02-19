c8f31a9a3a7e11e1fd092c731bfcfd5d52c9ec68
Gideon Goldberg <gidsgoldberg@gmail.com>
2018-02-19 17:55:04 +0000

    compute order from phases

--- app/models/register.rb
+++ app/models/register.rb
@@ -3,12 +3,14 @@ class Register < ApplicationRecord
 
   CURRENT_PHASES = %w[Backlog Discovery Alpha Beta].freeze
 
+  ordered_phases = "CASE register_phase #{CURRENT_PHASES.each_with_index.map { |phase, i| "WHEN '#{phase}' THEN #{i}" }.join(' ')} END"
+
   validates_presence_of :name, :register_phase
   validates_uniqueness_of :name
   validates :slug, uniqueness: true
 
   scope :by_name, -> { order name: :asc }
-  scope :sort_by_phase_name_asc, -> { order("CASE register_phase WHEN 'Beta' THEN 2 WHEN 'Alpha' THEN 3 WHEN 'Discovery' THEN 4 WHEN 'Backlog' THEN 5 END") }
+  scope :sort_by_phase_name_asc, -> { order(ordered_phases) }
   scope :sort_by_phase_name_desc, -> { order("CASE register_phase WHEN 'Backlog' THEN 1 WHEN 'Discovery' THEN 2 WHEN 'Alpha' THEN 3 WHEN 'Beta' THEN 4 END") }
   scope :has_records, -> { where(id: Record.select(:register_id)) }
   scope :available, -> { has_records.or(Register.where(register_phase: 'Backlog')) }
