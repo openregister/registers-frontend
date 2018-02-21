ab784489f42c7b9f8f23215a11a97fcdb29ae1e5
Dan Mitchell <dan@digitalreflow.co.uk>
2018-02-21 17:07:40 +0000

    Remove HTTP auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,4 @@ class ApplicationController < Spina::ApplicationController
   include Spina::Frontend
 
   protect_from_forgery with: :exception
-  http_basic_authenticate_with name: ENV['HTTP_AUTH_USERNAME'], password: ENV['HTTP_AUTH_PASSWORD'] if Rails.env.production?
 end
