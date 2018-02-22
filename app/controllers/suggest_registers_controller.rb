da5a454b9a8429ea63f38a3c0e235b411fca7117
Dan Mitchell <dan@digitalreflow.co.uk>
2018-02-22 23:41:51 +0000

    Lint all the things

--- app/controllers/suggest_registers_controller.rb
+++ app/controllers/suggest_registers_controller.rb
@@ -9,22 +9,18 @@ class SuggestRegistersController < ApplicationController
     @suggest_register = @wizard.object
 
     if @wizard.save
-      debugger
       redirect_to root_path
     else
       render :index
     end
   end
 
-  def thanks
+  def thanks; end
 
-  end
-
-  private
+private
 
   def suggest_register_params
     return params unless params[:suggest_register]
     params.require(:suggest_register).permit(:current_step, :email, :title, :reason)
   end
-
 end
