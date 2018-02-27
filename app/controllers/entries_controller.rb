52edb4689461a7b1827bef363b558da384078a1b
Dan Mitchell <dan@digitalreflow.co.uk>
2018-02-27 17:16:51 +0000

    Refactor entries pagination to use Kaminari

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -22,29 +22,24 @@ class EntriesController < ApplicationController
       { current_record: current_record, previous_record: previous_record, updated_fields: changed_fields, key: current_record.key }
     end
 
-    @entries_with_items = Kaminari::paginate_array(@entries_with_items, total_count: @result_count).page(@current_page).per(100)
+    @entries_with_items = Kaminari::paginate_array(@entries_with_items).page(params[:page]).per(100)
   end
 
 private
 
-  def recover_entries_history(register_id, fields, params, page_size = 100)
-    search_term = params[:q]
-    page = (params[:page] ||= 1).to_i
-
-    if search_term.present?
+  def recover_entries_history(register_id, fields, params)
+    if params[:q].present?
       partial = ''
       operation_params = []
       fields.split(',').each { |field| partial += " data->> '#{field}' ilike ? or" }
       partial = partial[1, partial.length - 3] # Remove beginning space and end 'or'
 
       operation_params.push(partial)
-      fields.split(',').count.times { operation_params.push("%#{search_term}%") }
+      fields.split(',').count.times { operation_params.push("%#{params[:q]}%") }
 
-      query = Entry.where(register_id: register_id, entry_type: 'user').where(operation_params).order(:entry_number).reverse_order.limit(page_size).offset(page_size * (page - 1))
-      count_query = Entry.where(register_id: register_id, entry_type: 'user').where(operation_params)
+      query = Entry.where(register_id: register_id, entry_type: 'user').where(operation_params).order(:entry_number).reverse_order
     else
-      query = Entry.where(register_id: register_id, entry_type: 'user').order(:entry_number).reverse_order.limit(100).offset(100 * (page - 1))
-      count_query = Entry.where(register_id: register_id, entry_type: 'user')
+      query = Entry.where(register_id: register_id, entry_type: 'user').order(:entry_number).reverse_order
     end
 
     previous_entries_numbers = query.reject { |entry| entry.previous_entry_number.nil? }.map(&:previous_entry_number)
@@ -54,9 +49,6 @@ private
       entries = previous_entries_query.select { |previous_entry| previous_entry.entry_number == entry.previous_entry_number }.first
       { current_entry: entry, previous_entry: entries }
     end
-    @result_count = count_query.count
-    @current_page = page
-    @total_pages = (@result_count / page_size) + ((@result_count % 100).zero? ? 0 : 1)
 
     result
   end
