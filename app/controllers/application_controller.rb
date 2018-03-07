d0a622dfb11095de6b08899879a4d69081a9e56b
Dan Mitchell <dan@digitalreflow.co.uk>
2018-03-07 10:07:05 +0000

    Add HTTP Auth back

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < Spina::ApplicationController
   include Spina::Frontend
 
+  http_basic_authenticate_with name: ENV['HTTP_AUTH_USERNAME'], password: ENV['HTTP_AUTH_PASSWORD'] if Rails.env.production?
+
   protect_from_forgery with: :exception
 end
