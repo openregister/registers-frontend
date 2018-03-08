9745887a7f7ee9ffa1af103995f26abd425bc5d4
Dan Mitchell <dan@digitalreflow.co.uk>
2018-03-08 12:26:41 +0000

    Add in progress registers collection page

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,8 @@ Rails.application.routes.draw do
     resources :entries, path: 'updates', only: :index
   end
 
+  get '/registers-in-progress', to: 'registers#in_progress'
+
   resources :suggest_registers, path: 'suggest-register', except: %i[show edit]
   get 'suggest-register/complete', to: 'suggest_registers#complete'
 
