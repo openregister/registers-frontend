22f2cdf5bba436643908b5351e012d597405b5ba
Dan Mitchell <dan@digitalreflow.co.uk>
2018-03-26 15:02:29 +0100

    Set API Key after validation

--- app/models/api_user.rb
+++ app/models/api_user.rb
@@ -1,15 +1,18 @@
 class ApiUser
   include ActiveModel::Model
+  include ActiveModel::Validations
+  include ActiveModel::Validations::Callbacks
 
   attr_accessor :email, :service, :department, :api_key
 
-  #before_save :set_api_key
-
   validates :email, presence: true
+
+  after_validation :set_api_key
 
 private
 
   def set_api_key
+    return unless errors.blank?
     if self.api_key.blank?
       self.api_key = SecureRandom.uuid
     end
