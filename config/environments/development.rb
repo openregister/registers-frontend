70a55929c01b1b4650b6922b8986178032d4fc32
Gideon Goldberg <gidsgoldberg@gmail.com>
2018-03-28 14:15:20 +0100

    move registration endpoint to config, refactor form POST

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -53,4 +53,5 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.self_service_api_endpoint = 'http://localhost:3001/users'
 end
