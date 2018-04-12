9ccda53c2c97f1c047d8d01dc609db3a733e2dd8
Gideon Goldberg <gidsgoldberg@gmail.com>
2018-04-12 11:59:34 +0100

    add error handling in api key post (#257)
    * add error handling in api key post
    
    * switch to httparty catch errors
    
    * add additional logging
    
    * use env.fetch
    
    * update docker compose

--- app/controllers/api_users_controller.rb
+++ app/controllers/api_users_controller.rb
@@ -1,6 +1,7 @@
 require 'net/http'
 require 'uri'
 require 'json'
+require 'httparty'
 
 class ApiUsersController < ApplicationController
   before_action :set_government_organisations
@@ -13,11 +14,12 @@ class ApiUsersController < ApplicationController
     @api_user = ApiUser.new(api_user_params)
     if @api_user.valid?
       response = post_to_endpoint(@api_user)
-      if response.is_a? Net::HTTPCreated
+      if response&.code == 201
         @api_key = JSON.parse(response.body)['api_key']
         render :show
       else
-        flash[:errors] = 'Something went wrong'
+        flash.alert = 'Something went wrong'
+        logger.error("API Key POST failed with unexpected response code: #{response.code}") if response&.code
         render :new
       end
     end
@@ -28,15 +30,19 @@ private
   def post_to_endpoint(user)
     @user = { email: user.email, department: user.department, service: user.service }
     uri = URI.parse(Rails.configuration.self_service_api_endpoint)
-    http = Net::HTTP.new(uri.host, uri.port)
-    http.use_ssl = (uri.scheme == 'https')
-
-    http.start do |http_start|
-      request = Net::HTTP::Post.new(uri.request_uri)
-      request.basic_auth(ENV['SELF_SERVICE_HTTP_AUTH_USERNAME'], ENV['SELF_SERVICE_HTTP_AUTH_PASSWORD'])
-      request.set_form_data(@user)
-      http_start.request(request)
+    options = {
+      basic_auth: { username: ENV.fetch['SELF_SERVICE_HTTP_AUTH_USERNAME'], password: ENV.fetch['SELF_SERVICE_HTTP_AUTH_PASSWORD'] },
+      body: @user
+    }
+    error_message = 'Something went wrong'
+    begin
+      HTTParty.post(uri, options)
     end
+  rescue StandardError => e
+    # Fallback for socket errors etc...
+    logger.error("API Key POST failed with exception: #{e}")
+    flash.alert = error_message
+    nil
   end
 
   def api_user_params
