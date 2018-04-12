b0ec5780f9a577fbdf7519c0d0c34c832d538bde
Gideon Goldberg <gidsgoldberg@gmail.com>
2018-04-12 13:01:02 +0100

    move credentials into config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,4 +84,6 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'https://registers.cloudapps.digital' }
   config.self_service_api_endpoint = 'https://registers-selfservice.cloudapps.digital/users'
+  config.self_service_http_auth_username = ENV.fetch('SELF_SERVICE_HTTP_AUTH_USERNAME')
+  config.self_service_http_auth_password = ENV.fetch('SELF_SERVICE_HTTP_AUTH_PASSWORD')
 end
