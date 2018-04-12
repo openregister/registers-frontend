6b596f78737992114f096eb1207bbc9d13084e6e
Gideon Goldberg <gidsgoldberg@gmail.com>
2018-04-12 13:01:02 +0100

    move credentials into config

--- app/controllers/api_users_controller.rb
+++ app/controllers/api_users_controller.rb
@@ -31,7 +31,7 @@ private
     @user = { email: user.email, department: user.department, service: user.service }
     uri = URI.parse(Rails.configuration.self_service_api_endpoint)
     options = {
-      basic_auth: { username: ENV.fetch['SELF_SERVICE_HTTP_AUTH_USERNAME'], password: ENV.fetch['SELF_SERVICE_HTTP_AUTH_PASSWORD'] },
+      basic_auth: { username: Rails.configuration.self_service_http_auth_username, password: Rails.configuration.self_service_http_auth_password },
       body: @user
     }
     error_message = 'Something went wrong'
