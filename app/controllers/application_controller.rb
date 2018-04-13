93556140cfed1b757ec29327061f1601bb972398
Gideon Goldberg <gidsgoldberg@gmail.com>
2018-04-13 11:31:09 +0100

    add uri check to authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,9 @@
 class ApplicationController < Spina::ApplicationController
   include Spina::Frontend
 
-  http_basic_authenticate_with name: ENV['HTTP_AUTH_USERNAME'], password: ENV['HTTP_AUTH_PASSWORD'] if Rails.env.production?
+  is_trial_domain = Rails.env.production? && JSON.parse(ENV.fetch('VCAP_APPLICATION'))['uris'].include?('registers-trial.service.gov.uk')
+
+  http_basic_authenticate_with name: ENV['HTTP_AUTH_USERNAME'], password: ENV['HTTP_AUTH_PASSWORD'] if is_trial_domain
 
   protect_from_forgery with: :exception
 
