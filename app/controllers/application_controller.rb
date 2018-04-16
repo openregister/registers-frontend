a2bace31c8f2cbca4da4931db6aa613a20a8dce8
Gideon Goldberg <gidsgoldberg@gmail.com>
2018-04-16 10:30:51 +0100

    move auth credentails to rails configuration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,7 @@ class ApplicationController < Spina::ApplicationController
   include Spina::Frontend
 
   is_trial_domain = Rails.env.production? && JSON.parse(ENV.fetch('VCAP_APPLICATION'))['uris'].include?('registers-trial.service.gov.uk')
-
-  http_basic_authenticate_with name: ENV['HTTP_AUTH_USERNAME'], password: ENV['HTTP_AUTH_PASSWORD'] if is_trial_domain
+  http_basic_authenticate_with name: Rails.configuration.http_auth_username, password: Rails.configuration.http_auth_password if is_trial_domain
 
   protect_from_forgery with: :exception
 
