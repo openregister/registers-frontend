198437d24fa8ce9b253d79ed65346b48032f33f9
Gideon Goldberg <gidsgoldberg@gmail.com>
2018-04-26 15:34:40 +0100

    display register-name

--- app/models/register.rb
+++ app/models/register.rb
@@ -58,6 +58,11 @@ class Register < ApplicationRecord
     Record.where(register_id: id, entry_type: 'user').count
   end
 
+  def register_name
+    Record.where(register_id: id, entry_type: 'system', key: 'register-name')
+          .pluck("data -> 'register-name' as register_name").first || name
+  end
+
 private
 
   def set_slug
