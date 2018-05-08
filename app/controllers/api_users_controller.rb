597a12471fd41eedac654816df9f9490221bf78c
Gideon Goldberg <gidsgoldberg@gmail.com>
2018-05-08 15:47:48 +0100

    use official name and send curie as ID

--- app/controllers/api_users_controller.rb
+++ app/controllers/api_users_controller.rb
@@ -72,9 +72,20 @@ private
   end
 
   def set_government_orgs_local_authorities
-    @government_orgs_local_authorities = Register.find_by(slug: ['government-organisation', 'local-authority-eng', 'local-authority-sct', 'local-authority-nir', 'principal-local-authority'])
-                                        .records
-                                        .where(entry_type: 'user')
-                                        .current
+    registers = {
+        'government-organisation': 'name',
+        'local-authority-eng': 'official-name',
+        'local-authority-sct': 'official-name',
+        'local-authority-nir': 'official-name',
+        'principal-local-authority': 'official-name',
+    }
+
+    @government_orgs_local_authorities = registers.map { |k, v|
+      Register.find_by(slug: k)&.records&.where(entry_type: 'user')&.current&.map { |r|
+        [
+            r.data[v], "#{k}:#{r.key}"
+        ]
+      }
+    }.compact.flatten(1)
   end
 end
