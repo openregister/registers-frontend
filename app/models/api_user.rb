e7cabd7319d7657278ef9200b16dfcf16c3bd015
Gideon Goldberg <gidsgoldberg@gmail.com>
2018-05-11 10:18:52 +0100

    use lambda for conditional to fix rails warning

--- app/models/api_user.rb
+++ app/models/api_user.rb
@@ -3,8 +3,8 @@ class ApiUser
   attr_accessor :email_gov, :email_non_gov, :non_gov_use_category, :department, :is_government
 
   validates :is_government, presence: true
-  validates :email_gov, presence: true, if: "is_government == 'yes'"
-  validates :department, presence: true, if: "is_government == 'yes'"
-  validates :email_non_gov, presence: true, if: "is_government == 'no'"
-  validates :non_gov_use_category, presence: true, if: "is_government == 'no'"
+  validates :email_gov, presence: true, if: -> { is_government == 'yes' }
+  validates :department, presence: true, if: -> { is_government == 'yes' }
+  validates :email_non_gov, presence: true, if: -> { is_government == 'no' }
+  validates :non_gov_use_category, presence: true, if: -> { is_government == 'no' }
 end
