068e8d253d2afc3ec38df3cdee7c96b97ef9ec2b
Gideon Goldberg <gidsgoldberg@gmail.com>
2018-05-17 10:39:03 +0100

    simplify validation logic

--- app/models/concerns/form_validations.rb
+++ app/models/concerns/form_validations.rb
@@ -1,13 +1,14 @@
 require 'active_support/concern'
 
-module FormValidations extend ActiveSupport::Concern
-                       included do
-                         validates :is_government, inclusion: { in: [true, false] }
-                         validates :email_gov, presence: true, if: -> { is_government == true }
-                         validates :department, presence: true, if: -> { is_government == true  }
-                         validates :department, absence: true, if: -> { is_government == false  }
-                         validates :email_non_gov, presence: true, if: -> { is_government == false }
-                         validates :non_gov_use_category, presence: true, if: -> { is_government == false }
-                         validates :non_gov_use_category, absence: true, if: -> { is_government == true }
-                       end
+module FormValidations
+  extend ActiveSupport::Concern
+  included do
+    validates :is_government, inclusion: { in: [true, false] }
+    validates :email_gov, presence: true, if: -> { is_government }
+    validates :department, presence: true, if: -> { is_government }
+    validates :department, absence: true, unless: -> { is_government }
+    validates :email_non_gov, presence: true, unless: -> { is_government }
+    validates :non_gov_use_category, presence: true, unless: -> { is_government }
+    validates :non_gov_use_category, absence: true, if: -> { is_government }
+  end
 end
