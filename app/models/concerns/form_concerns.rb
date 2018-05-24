0b5516306cafcea1ba5cb1ca4c8c959a75c989b6
Gideon Goldberg <gidsgoldberg@gmail.com>
2018-05-24 17:10:09 +0100

    validate email address on server-side

--- app/models/concerns/form_concerns.rb
+++ app/models/concerns/form_concerns.rb
@@ -10,6 +10,9 @@ module FormConcerns
     validates :email_non_gov, presence: true, unless: -> { is_government }
     validates :non_gov_use_category, presence: true, if: -> { is_government == false }
     validates :non_gov_use_category, absence: true, if: -> { is_government }
+    validates_format_of :email_gov, :email_non_gov,
+  with: /\A(|(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,})\z/i
+
 
     def email
       is_government ? email_gov : email_non_gov
