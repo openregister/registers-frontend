0370bfa2f1dcc6b09037534c6ef374b303f67656
Gideon Goldberg <gidsgoldberg@gmail.com>
2018-05-25 12:08:12 +0100

    add test for register-name

--- spec/features/view_register_spec.rb
+++ spec/features/view_register_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 RSpec.feature 'View register', type: :feature do
   before(:all) do
     country_data = File.read('./spec/support/country.rsf')
+    industrial_classification_data = File.read('./spec/support/industrial-classification-2003.rsf')
+
     stub_request(:get, "https://country.register.gov.uk/download-rsf/0").
     with(headers: { 'Accept' => '*/*', 'Accept-Encoding' => 'gzip, deflate', 'Host' => 'country.register.gov.uk' }).
     to_return(status: 200, body: country_data, headers: {})
@@ -17,8 +19,26 @@ RSpec.feature 'View register', type: :feature do
     with(headers: { 'Accept' => '*/*', 'Accept-Encoding' => 'gzip, deflate', 'Host' => 'country.register.gov.uk' }).
     to_return(body: country_proof)
 
+    stub_request(:get, "https://industrial-classification-2003.cloudapps.digital/download-rsf/0").
+    with(headers: {
+      'Accept' => '*/*',
+      'Accept-Encoding' => 'gzip, deflate',
+      'Host' => 'industrial-classification-2003.cloudapps.digital',
+      }).
+    to_return(status: 200, body: industrial_classification_data)
+
+    stub_request(:get, "https://industrial-classification-2003.cloudapps.digital/download-rsf/1158").
+    with(headers: {
+      'Accept' => '*/*',
+      'Accept-Encoding' => 'gzip, deflate',
+      'Host' => 'industrial-classification-2003.cloudapps.digital'
+      }).
+    to_return(status: 200, body: "assert-root-hash\tsha-256:2183de14afa34180a48c274e60d06841facf4627afd769b7f4cf8bf4457a3129")
+
     country = ObjectsFactory.new.create_register('Country', 'Beta', 'D587')
+    industrial_classification = ObjectsFactory.new.create_register('industrial classification 2003', 'Discovery', 'D587')
     PopulateRegisterDataInDbJob.perform_now(country)
+    PopulateRegisterDataInDbJob.perform_now(industrial_classification)
   end
 
   scenario 'view and sort a register' do
@@ -45,6 +65,13 @@ RSpec.feature 'View register', type: :feature do
     expect(first_update.text).to eq('CI')
     expect(first_update.sibling('table')).to have_content("The Republic of Côte D’Ivoire")
     expect(first_update.sibling('table')).to have_content("The Republic of Cote D'Ivoire")
+  end
+
+  scenario 'view register name' do
+    visit('/registers/industrial-classification-2003')
+    register_name = 'UK Standard Industrial Classification of Economic Activities 2003 register'
+    expect(find('h1')).to have_content(register_name)
+    expect(page.title).to have_content(register_name)
   end
 
   after(:all) do
