3f538359b53fd2a231b15b68884866587c4d2545
Dan Mitchell <dan@digitalreflow.co.uk>
2018-05-31 13:10:51 +0100

    Remove department and non_gov_use_category

--- app/models/api_user.rb
+++ app/models/api_user.rb
@@ -2,5 +2,5 @@ class ApiUser
   include ActiveModel::Model
   include ActiveModel::Translation
   include FormConcerns
-  attr_accessor :email_gov, :email_non_gov, :non_gov_use_category, :department, :is_government
+  attr_accessor :email_gov, :email_non_gov, :is_government
 end
