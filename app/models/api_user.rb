f402aeaf0276552c74bdfc3fd44e0371e3871009
Gideon Goldberg <gidsgoldberg@gmail.com>
2018-06-01 13:42:00 +0100

    move contactable logic out of shared concern

--- app/models/api_user.rb
+++ app/models/api_user.rb
@@ -3,4 +3,15 @@ class ApiUser
   include ActiveModel::Translation
   include FormConcerns
   attr_accessor :email_gov, :email_non_gov, :is_government, :contactable
+
+  validates :contactable, presence: true, if: -> { is_government_boolean == true }
+
+  def is_contactable_boolean
+    case contactable
+    when 'yes'
+      true
+    when 'no'
+      false
+    end
+  end
 end
--- app/models/concerns/form_concerns.rb
+++ app/models/concerns/form_concerns.rb
@@ -6,7 +6,6 @@ module FormConcerns
     validates :is_government, presence: true
     validates :email_gov, presence: true, email: true, if: -> { is_government_boolean == true }
     validates :email_non_gov, allow_blank: true, email: true, if: -> { is_government_boolean == false }
-    validates :contactable, presence: true, if: -> { is_government_boolean == true }
 
     def email
       is_government_boolean ? email_gov : email_non_gov
@@ -14,15 +13,6 @@ module FormConcerns
 
     def is_government_boolean
       case is_government
-      when 'yes'
-        true
-      when 'no'
-        false
-      end
-    end
-
-    def is_contactable_boolean
-      case contactable
       when 'yes'
         true
       when 'no'
