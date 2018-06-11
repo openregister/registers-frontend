62f1f66918831150e2f5a801f0845161b76d64c9
Dan Mitchell <dan@danmitchell.co.uk>
2018-06-11 16:22:02 +0100

    Remove backlog registers (#345)
    * Remove backlog registers from UI
    * Redirect request suggest flows and redirect to support

--- app/models/new_register.rb
+++
@@ -1,7 +0,0 @@
-class NewRegister
-  include ActiveModel::Model
-
-  attr_accessor :email, :name, :message, :subject
-
-  validates_presence_of :email, :name, :message
-end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ Rails.application.routes.draw do
 
   get 'using-registers', to: 'pages#using_registers', as: 'using_registers'
   get 'services-using-registers', to: 'pages#services_using_registers', as: 'services_using_registers'
-  get 'avaliable-registers', to: redirect('/registers', status: 301)
   get 'combining-registers', to: 'pages#combining_registers', as: 'combining_registers'
   get 'case-study-tiscreport', to: 'pages#case_study', as: 'case_study'
   get 'privacy-notice', to: 'pages#privacy_notice', as: 'privacy_notice'
@@ -19,19 +18,11 @@ Rails.application.routes.draw do
     get '/download-csv', to: 'download#download_csv', as: 'download_csv'
   end
 
-  resources :api_users, path: 'create-api-key'
-  get '/api_users/new', to: redirect('/create-api-key', status: 301)
-
   get '/registers-in-progress', to: 'registers#in_progress'
 
-  resources :suggest_registers, path: 'suggest-register', except: %i[show edit]
-  get 'suggest-register/complete', to: 'suggest_registers#complete'
-
-  resources :request_registers, path: 'request-register', except: %i[show edit]
-  get 'request-register/complete', to: 'request_registers#complete'
+  resources :api_users, path: 'create-api-key'
 
   # Support
-
   get 'support', to: 'support#index'
   post 'select_support', to: 'support#select_support'
 
@@ -43,21 +34,21 @@ Rails.application.routes.draw do
 
   get 'support/thanks', to: 'support#thanks'
 
-  # Request or suggest register
-
-  get 'new-register', to: 'new_register#index'
-  post 'select_request_or_suggest', to: 'new_register#select_request_or_suggest'
-
-  get 'new-register/suggest-register', to: 'new_register#suggest_register'
-  post 'new-register/suggest-register', to: 'new_register#create'
-
-  get 'new-register/request-register', to: 'new_register#request_register'
-  post 'new-register/request-register', to: 'new_register#create'
-
-  get 'new-register/thanks', to: 'new_register#thanks'
-
   resource :sitemap, only: :show
 
+  # Legacy URL's so we dont break users
+  get 'suggest-register', to: redirect('/support', status: 301)
+  get 'request-register', to: redirect('/support', status: 301)
+  get 'new-register', to: redirect('/support', status: 301)
+  get 'new-register/request-register', to: redirect('/support', status: 301)
+  get 'new-register/suggest-register', to: redirect('/support', status: 301)
+  get 'api_users/new', to: redirect('/create-api-key', status: 301)
+  get 'avaliable-registers', to: redirect('/registers', status: 301)
+
+  get '404', to: 'errors#not_found'
+  get '500', to: 'errors#internal_server_error'
+
+  # Admin paths
   namespace :admin, path: '/admin' do
     root to: "registers#index"
 
@@ -66,13 +57,13 @@ Rails.application.routes.draw do
         patch :sort
       end
     end
+
     resources :users
+
     resources :sessions
     get "signin" => "sessions#new"
     get "signout" => "sessions#destroy"
+
     resources :password_resets
   end
-
-  get '404', to: 'errors#not_found'
-  get '500', to: 'errors#internal_server_error'
 end
