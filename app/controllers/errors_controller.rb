75594ebe3cc9f7d15098e6f1881562d8323b8556
Gideon Goldberg <gidsgoldberg@gmail.com>
2018-06-28 12:13:20 +0100

    handle non html format errors

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,9 +1,15 @@
 class ErrorsController < ApplicationController
   def not_found
-    render status: 404
+    respond_to do |format|
+      format.html { render status: 404 }
+      format.any { head :not_found }
+    end
   end
 
   def internal_server_error
-    render status: 500
+    respond_to do |format|
+      format.html { render status: 500 }
+      format.any { head :internal_server_error }
+    end
   end
 end
