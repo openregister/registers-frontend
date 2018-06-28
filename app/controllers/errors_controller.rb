6679f36904bbde2776d291983782e277b6792ba7
Gideon Goldberg <gidsgoldberg@gmail.com>
2018-06-28 14:52:45 +0100

    handle non html format errors (#371)
    * handle non html format errors
    
    * use symbol for error code

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,9 +1,15 @@
 class ErrorsController < ApplicationController
   def not_found
-    render status: 404
+    respond_to do |format|
+      format.html { render status: :not_found }
+      format.any { head :not_found }
+    end
   end
 
   def internal_server_error
-    render status: 500
+    respond_to do |format|
+      format.html { render status: :internal_server_error }
+      format.any { head :internal_server_error }
+    end
   end
 end
