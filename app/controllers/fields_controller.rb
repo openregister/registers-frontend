b78358a4d8fa04e951b31b35ad140829b2e89dff
Gideon Goldberg <gidsgoldberg@gmail.com>
2018-07-11 09:59:17 +0100

    fix 500 when field not found

--- app/controllers/fields_controller.rb
+++ app/controllers/fields_controller.rb
@@ -1,7 +1,6 @@
 class FieldsController < ApplicationController
   def show
     @register = Register.find_by_slug!(params[:register_id])
-
-    @field = Record.where(register_id: @register.id, key: "field:#{params[:id]}").first
+    @field = Record.find_by!(register_id: @register.id, key: "field:#{params[:id]}")
   end
 end
