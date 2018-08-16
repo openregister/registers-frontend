6681d8abd2dc23c02609b36bc21c977bd84e8ff5
Mat Moore <matmoore@users.noreply.github.com>
2018-08-16 12:05:59 +0100

    Wrap raw SQL with Arel.sql
    Passing strings directly is deprecated and will be removed in rails 6.

--- app/models/record.rb
+++ app/models/record.rb
@@ -3,8 +3,8 @@ class Record < ApplicationRecord
 
   include SearchScope
   belongs_to :register
-  scope :current, -> { where("data->> 'end-date' is null") }
-  scope :archived, -> { where("data->> 'end-date' is not null") }
+  scope :current, -> { where(Arel.sql("data->> 'end-date' is null")) }
+  scope :archived, -> { where(Arel.sql("data->> 'end-date' is not null")) }
   scope :status, lambda { |status|
     case status
     when 'archived', 'current'
