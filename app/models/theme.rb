5a8632c5dd4df8cfe807508148862756f13e0e57
Ian James <ian.james@digital.cabinet-office.gov.uk>
2018-09-11 09:27:31 +0100

    Tweaks to pass linting.

--- app/models/theme.rb
+++ app/models/theme.rb
@@ -3,20 +3,20 @@ class Theme < ApplicationRecord
 
   scope :themes, -> { Theme.all }
 
-  scope :registers_in_a_theme, -> (theme_id){
+  scope :registers_in_a_theme, ->(theme_id) {
     Register.where(theme_id: theme_id)
   }
-  scope :count_the_registers_in_a_theme, -> (theme_id){
+  scope :count_the_registers_in_a_theme, ->(theme_id) {
     registers_in_a_theme(theme_id).count
   }
-  scope :count_of_registers_human_readable, -> (theme_id) {
+  scope :count_of_registers_human_readable, ->(theme_id) {
     count = registers_in_a_theme(theme_id).count
-    "#{count} #{(count == 1) ? 'register' : 'registers'}"
+    "#{count} #{count == 1 ? 'register' : 'registers'}"
   }
 
   scope :themes_and_count, -> {
     themes.map { |t|
-      themes_counted = {
+      {
         name: t.name,
         id: t.id,
         taxon_content_id: t.taxon_content_id,
