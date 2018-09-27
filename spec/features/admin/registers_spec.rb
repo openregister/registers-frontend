dfa76fe8e19e7b771492e811f2c67255276954ea
Gideon Goldberg <gidsgoldberg@gmail.com>
2018-09-27 11:19:19 +0100

    add test

--- spec/features/admin/registers_spec.rb
+++ spec/features/admin/registers_spec.rb
@@ -1,15 +1,29 @@
 require 'rails_helper'
 
 RSpec.feature "Admin registers", type: :feature do
-  describe 'listing registers' do
+  describe 'updating registers' do
     let!(:user) { FactoryBot.create :user }
     let!(:register) { FactoryBot.create_list :register, 3 }
 
-    before { sign_in(User.first) }
+    before do
+      sign_in(User.first)
+      ObjectsFactory.new.create_authority('Foreign & Commonwealth Office', 'D13')
+    end
 
     it 'shows all the registers' do
       visit '/admin/registers'
       expect(page).to have_css '.sortable li', count: 3
+    end
+
+    it 'Updates a register\'s authority' do
+      job = class_double(PopulateRegisterDataInDbJob).
+      as_stubbed_const(perform_later: true)
+      expect(job).to receive(:perform_later)
+      register_id = Register.first.id
+      visit "/admin/registers/#{register_id}/edit"
+      select 'Foreign & Commonwealth Office', from: 'Authority'
+      click_button 'Save'
+      expect(Register.first.authority.name).to eq('Foreign & Commonwealth Office')
     end
   end
 end
