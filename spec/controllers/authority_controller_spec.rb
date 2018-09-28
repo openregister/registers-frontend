f68680782ea81e6e2b6cd1dfdc36b4e0c3ae387f
Ian James <ian.james@digital.cabinet-office.gov.uk>
2018-09-28 16:03:13 +0100

    Tests for 301 redirect to correct location.

--- spec/controllers/authority_controller_spec.rb
+++ spec/controllers/authority_controller_spec.rb
@@ -1,17 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe AuthorityController, type: :controller do
-  describe "GET #index" do
-    it "returns http success" do
+  describe "Categories index page" do
+    it "returns a 301 redirect" do
       get :index
       expect(response).to have_http_status(301)
     end
+    it "redirects to the registers collection page with Organisation selected" do
+      get :index
+      expect(response.location).to end_with registers_path(show_by: "organisation")
+    end
   end
-
-  # describe "GET #show" do
-  #   it "returns http success" do
-  #     get :show
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
 end
--- spec/controllers/category_controller_spec.rb
+++ spec/controllers/category_controller_spec.rb
@@ -1,17 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe CategoryController, type: :controller do
-  describe "GET #index" do
-    it "returns http success" do
+  describe "Category index page" do
+    it "returns a 301 redirect" do
       get :index
       expect(response).to have_http_status(301)
     end
+    it "redirects to the registers collection page" do
+      get :index
+      expect(response.location).to end_with registers_path
+    end
   end
-
-  # describe "GET #show" do
-  #   it "returns http success" do
-  #     get :show
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
 end
