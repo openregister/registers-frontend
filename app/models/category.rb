3af15216dea2995a35518625d0f8d93d0b5810a2
Ian James <ian.james@digital.cabinet-office.gov.uk>
2018-10-02 13:50:43 +0100

    Changes homepage category scope.

--- app/models/category.rb
+++ app/models/category.rb
@@ -8,13 +8,11 @@ class Category < ApplicationRecord
   }
 
   scope :with_a_register__shown_on_homepage, -> {
-    Category.with_a_register.reject { |r|
-      r.slug == 'digital-data-and-technology-profession-capability-framework'
-    }
+    with_a_register.where.not(slug: 'digital-data-and-technology-profession-capability-framework')
   }
 
   scope :collection, ->(slug) {
-    Category.find_by!(slug: slug)
+    find_by!(slug: slug)
   }
 
   def registers_by_this_collection
