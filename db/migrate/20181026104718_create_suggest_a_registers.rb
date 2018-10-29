8795898f4b81fe39988682932203501b7b81337a
Ian James <ian.james@digital.cabinet-office.gov.uk>
2018-10-29 16:19:32 +0000

    Removes unrequired database changes.

--- db/migrate/20181026104718_create_suggest_a_registers.rb
+++
@@ -1,7 +0,0 @@
-class CreateSuggestARegisters < ActiveRecord::Migration[5.2]
-#   def change
-#     create_table :suggest_a_registers do |t|
-#       t.timestamps
-#     end
-#   end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_26_104718) do
+ActiveRecord::Schema.define(version: 2018_09_24_161540) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -95,11 +95,6 @@ ActiveRecord::Schema.define(version: 2018_10_26_104718) do
     t.boolean "featured", default: false
     t.bigint "category_id"
     t.bigint "authority_id"
-  end
-
-  create_table "suggest_a_registers", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "users", id: :serial, force: :cascade do |t|
