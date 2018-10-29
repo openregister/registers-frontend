a760a884c5bc058d4fb822a1ba93a1e1b3db9dab
Ian James <ian.james@digital.cabinet-office.gov.uk>
2018-10-29 11:01:26 +0000

    Fixes migration error.

--- db/migrate/20181026104718_create_suggest_a_registers.rb
+++ db/migrate/20181026104718_create_suggest_a_registers.rb
@@ -1,7 +1,7 @@
-# class CreateSuggestARegisters < ActiveRecord::Migration[5.2]
+class CreateSuggestARegisters < ActiveRecord::Migration[5.2]
 #   def change
 #     create_table :suggest_a_registers do |t|
 #       t.timestamps
 #     end
 #   end
-# end
+end
