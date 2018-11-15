9d45b73959a0944d6572b81a00834f665fbb75bc
Gideon Goldberg <gidsgoldberg@gmail.com>
2018-11-15 16:40:22 +0000

    set CD2

--- app/services/register_records_downloader.rb
+++ app/services/register_records_downloader.rb
@@ -39,6 +39,7 @@ private
       aip: 1,
       ni: 1,
       dl: Rails.application.routes.url_helpers.register_download_ods_path(register.slug),
+      cd2: 'N/A',
       cd4: 'Registers-Frontend-Downloads'
     }
     HTTParty.post('https://www.google-analytics.com/collect', body: params)
