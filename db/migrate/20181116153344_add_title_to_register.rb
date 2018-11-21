87874a3e2b2b45a1b78d4d6977cca454964ed857
Ian James <ian.james@digital.cabinet-office.gov.uk>
2018-11-21 14:04:28 +0000

    Adds safe operators; populates title with fallback option on migration.

--- db/migrate/20181116153344_add_title_to_register.rb
+++ db/migrate/20181116153344_add_title_to_register.rb
@@ -1,5 +1,10 @@
 class AddTitleToRegister < ActiveRecord::Migration[5.2]
   def change
     add_column :registers, :title, :string
+
+    Register.find_each do |r|
+      r.title = r.name.humanize.gsub(/-/, ' ')
+      r.save
+    end
   end
 end
